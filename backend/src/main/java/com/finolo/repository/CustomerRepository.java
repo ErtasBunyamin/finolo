e37eda4dac7fb4e8330ac49ac26f80d9ed5c4746
Bünyamin Ertas <79013103+ErtasBunyamin@users.noreply.github.com>
2025-06-09 19:20:00 +0300

    Add recent operations feature

--- backend/src/main/java/com/finolo/repository/CustomerRepository.java
+++ backend/src/main/java/com/finolo/repository/CustomerRepository.java
@@ -10,4 +10,5 @@ public interface CustomerRepository extends JpaRepository<Customer, Long> {
     List<Customer> findByUser(User user);
     List<Customer> findAllByUser_Email(String email);
     int countByUser(User user);
+    List<Customer> findTop5ByUserOrderByIdDesc(User user);
 }
